ff67170f1b5aa8e3fbb9269d2b98bd0b9846b501
Abin Alias <abin76084@gmail.com>
2025-06-08 15:00:08 +1000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,67 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+import React from "react";
+
+export default function Portfolio() {
+  return (
+    <div
+      style={{
+        width: "100vw",
+        height: "100vh",
+        backgroundImage:
+          "url('https://images.unsplash.com/photo-1503264116251-35a269479413')",
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        color: "white",
+        padding: "2rem",
+        fontFamily: "monospace",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        textAlign: "center",
+      }}
+    >
+      <h1 style={{ fontSize: "2.5rem", marginBottom: "1rem" }}>Abin Alias</h1>
+      <p style={{ maxWidth: "600px", fontSize: "1.2rem", lineHeight: "1.8" }}>
+        Hello and welcome! I am currently studying Information Technology at
+        Queensland University of Technology in Brisbane. My interests include
+        software development, web design, and other creative tech projects!
+        <br /><br />
+        Take a look at my work through the tabs above :D
+      </p>
+
+      <nav style={{ marginTop: "2rem" }}>
+        <a
+          href="#projects"
+          style={{ margin: "0 1rem", color: "#fff", textDecoration: "underline" }}
+        >
+          Projects
+        </a>
+        <a
+          href="#skills"
+          style={{ margin: "0 1rem", color: "#fff", textDecoration: "underline" }}
+        >
+          Skills
+        </a>
+        <a
+          href="#contact"
+          style={{ margin: "0 1rem", color: "#fff", textDecoration: "underline" }}
+        >
+          Contact
+        </a>
+        <a
+          href="https://www.linkedin.com/in/abin-alias-08541a346"
+          target="_blank"
+          rel="noreferrer"
+          style={{ margin: "0 1rem", color: "#fff", textDecoration: "underline" }}
+        >
+          LinkedIn
+        </a>
+      </nav>
+    </div>
+  );
+}
